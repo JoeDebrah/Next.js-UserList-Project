04370644fb1567f088c2599a67134e210c84a1cd
Joe Debrah <joedebrah@macbook-air.myfiosgateway.com>
2023-09-20 16:02:56 -0400

    Added final files

--- foreach.js
+++
@@ -1,9 +0,0 @@
-let a = ["ant", "bat", "cat", 42];
-a.forEach( function(element) {
-    console.log(element);
-} );
-
-let solo = "To be or not to be, that is the question";
-Array.from(solo).forEach( function(charactor) {
-    console.log(charactor);
-});
